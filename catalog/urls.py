ef193a01ad979db60c2120599015eefd177b184c
Konsta <panfilov2507@gmail.com>
2021-02-27 23:41:02 +0300

    html, css рефакторинг. Реализована функция поиска (по ФИО и названию отдела). Добавлена странца с результатами поиска.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from catalog.views import IndexView, EmployeeDetail, GovernanceView
+from catalog.views import IndexView, EmployeeDetail, GovernanceView, ResultsView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     path('', include('django.contrib.auth.urls')),
     path('employee/<int:pk>', EmployeeDetail.as_view(), name='employee'),
     path('governance/', GovernanceView.as_view(), name='governance'),
+    path('results/', ResultsView.as_view(), name='results'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -4,8 +4,6 @@ from django.views import generic
 from .models import Employee, AdressDepartment, Department
 from django.contrib.auth.models import User, Group
 
-# Create your views here.
-
 
 class IndexView(View):
     """
@@ -19,7 +17,6 @@ class IndexView(View):
 
         # num_visits = request.session.get('num_visits', 0)
         # request.session['num_visits'] = num_visits + 1
-
 
         employees = Employee.objects.all()
         departments = Department.objects.all()
@@ -35,6 +32,41 @@ class GovernanceView(View):
         departments = Department.objects.filter(name='Руководство')
         context = {"employees": employees, "departments": departments}
         return render(request, 'catalog/index.html', context)
+
+
+class ResultsView(View):
+    def check_find(self, request):
+        employees = Employee.objects.all()
+        departments = Department.objects.all()
+        search = request.GET.get('search')
+
+        if search == "":
+            context = {"employees": employees, "departments": departments}
+            template = 'catalog/index.html'
+        else:
+            objects_employess = []
+            objects_departments = []
+
+            for employee in employees:
+                if (search.lower() in employee.surname.lower() or
+                search.lower() in employee.name.lower() or
+                search.lower() in employee.patronymic.lower()):
+
+                    objects_employess.append(employee)
+
+            for department in departments:
+                if search.lower() in department.name.lower():
+                    objects_departments.append(department)
+
+            context = {"employees": objects_employess, "departments": objects_departments,
+                       "count_emp": len(objects_employess), "count_dep": len(objects_departments)}
+            template = 'catalog/results.html'
+
+        return context, template
+
+    def get(self, request):
+        context, template = self.check_find(request)
+        return render(request, template, context)
 
 
 class EmployeeDetail(generic.DetailView):

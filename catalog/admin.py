6d16b8453beaf62f2487aac26c10a3757d09369e
konsta78 <panfilov2507@gmail.com>
2021-03-10 16:42:18 +0300

    Расширено отображение в админке

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from catalog.models import Department, Employee, AdressDepartment, SubdivisionDepartament
 
-# Register your models here.
 
-admin.site.register(Department)
-admin.site.register(Employee)
-admin.site.register(AdressDepartment)
-admin.site.register(SubdivisionDepartament)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'short_name', 'adress')
+    list_filter = ('adress',)
+
+admin.site.register(Department, DepartmentAdmin)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('surname', 'name', 'patronymic', 'position', 'department', 'email')
+    list_filter = ('department', )
+
+
+@admin.register(AdressDepartment)
+class AddressDepartmentAdmin(admin.ModelAdmin):
+    list_display = ('street', 'building', 'letter', 'room', 'id')
+
+
+@admin.register(SubdivisionDepartament)
+class SubdivisionDepartamentAdmin(admin.ModelAdmin):
+    pass
